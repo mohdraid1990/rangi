0de23f0dd0ab7bf894af61916cfc049e42c3b507
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-02-23 21:43:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+        //  Togle-menu
+         
+         function toggleMenu() {
+                const menu = document.querySelector('.menu');
+                menu.classList.toggle('show-menu');
+            }
